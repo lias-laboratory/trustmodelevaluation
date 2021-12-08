a2fb4f9461bf14a59f6bcb38eea4c40b5250c41b
SELLAMI Chayma <chayma.sellami@ensma.fr>
2021-12-08 16:21:36 +0000

    Update ComputationalEvaluationTest.java

--- src/test/java/fr/ensma/lias/trustevaluation/engine/ComputationalEvaluationTest.java
+++ src/test/java/fr/ensma/lias/trustevaluation/engine/ComputationalEvaluationTest.java
@@ -61,6 +61,40 @@ public class ComputationalEvaluationTest {
 	
 	@Test
 	public void evaluateScenarioWithBayesienNetworkWithMachineLearningFunctionTest() {
+		// Requirement{score:[-100..100]}:(Abstract Task{null})^1>>[(Positive Task{score>=50})^3;(Negative Task{score<50})^2;(Negative Task{score<50})^4]
+
+		// Given
+		ScoreElement scoreElement = new ScoreElement(-100, 100);
+		Task abstractTask = new AbstractTask("Abstract Task");
+		abstractTask.setDecomposition(Decomposition.SEQ);
+
+		Requirement current = new Requirement(abstractTask, scoreElement);
+		
+		Task positiveTask = new PositiveTask("Positive Task");
+		TaskScoreConstraint positiveTaskConstraint = new TaskScoreConstraint(scoreElement, new GreaterEqual(), new ExactScoreValue(50));
+		positiveTask.setIteration(3);
+		positiveTask.setConstraint(positiveTaskConstraint);
+		Task negativeTask1 = new NegativeTask("Negative Task");
+		negativeTask1.setIteration(2);
+		TaskScoreConstraint negativeTaskConstraint1 = new TaskScoreConstraint(scoreElement, new Lower(), new ExactScoreValue(50));
+		negativeTask1.setConstraint(negativeTaskConstraint1);
+		Task negativeTask2 = new NegativeTask("Negative Task");
+		negativeTask2.setIteration(4);
+		TaskScoreConstraint negativeTaskConstraint2 = new TaskScoreConstraint(scoreElement, new Lower(), new ExactScoreValue(50));
+		negativeTask2.setConstraint(negativeTaskConstraint2);
+
+		abstractTask.addTask(positiveTask);
+		abstractTask.addTask(negativeTask1);
+		abstractTask.addTask(negativeTask2);
+
+		// When
+		UserRequirementsEngine userEngine = new UserRequirementsEngine();
+		Scenario eval = userEngine.eval(current);
+		ComputationalEvaluationEngine ceEngine = new ComputationalEvaluationEngine();
+		ReportEvaluation reportEvaluation = ceEngine.eval(eval, new BayesienNetworkComputationalModel(), new MachineLearningFunction());
+
+		// Then
+		Assert.assertTrue(reportEvaluation.getScore() > 50);
 		
 	}
 }
