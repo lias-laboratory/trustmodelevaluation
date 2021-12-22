8e182085a1a569e48e2b6d44d2af89f9ba9286aa
Mickael BARON <baron@ensma.fr>
2021-12-22 09:32:23 +0100

    Add specific test class for RCISPaper usecase

---
+++ src/test/java/fr/ensma/lias/trustevaluation/casestudies/RCISPaper.java
@@ -0,0 +1,15 @@
+package fr.ensma.lias.trustevaluation.casestudies;
+
+import org.junit.Test;
+
+public class RCISPaper {
+
+	@Test
+	public void allUseCase() {
+		// Define all ApplicationRequirement.
+		
+		// Define all TrustRequirement based on the previous ApplicationRequirement objects.
+		
+		// ComputationalModel recommandation.
+	}	
+}
